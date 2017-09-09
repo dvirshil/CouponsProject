048ff6a301c259cdc4c5aee9b11a100d1e2db5c0
unknown <dvir.shil@gmail.comgit config --global user.name dvir_shil>
2017-09-09 22:06:30 +0300

    secound upload

---
+++ src/main/java/com/project/CouponsProject/Controller/Controller.java
@@ -0,0 +1,16 @@
+package com.project.CouponsProject.Controller;
+
+import javax.ws.rs.Path;
+
+import com.project.CouponsProject.Servlet.Servlet;
+
+@Path("login")
+public class Controller {
+	
+	
+	public static void login(String username, String password,String type) {
+		Servlet sr=new Servlet();
+	
+	}
+
+}
